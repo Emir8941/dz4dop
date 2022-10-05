ee2e8570fb35110dc6282d62b2568a62b798922f
Emir <emirsagyndykov931@gmail.com>
2022-10-05 19:58:15 +0600

    first commtit

---
+++ main.js
@@ -0,0 +1,48 @@
+// const body = document.body
+// body.innerHTML = `<ul><li>green</li></ul>`
+
+
+
+// let a = document.querySelectorAll('li')
+// let ul = document.querySelector('ul')
+// let o = Array.from( Object.values(a))
+// let d =  o.forEach((item)=>{
+//   return(  ul.innerHTML=`
+//     <li>${item}</li>
+//     `)
+// })
+// console.log(o);
+
+
+
+// const listitem = document.querySelectorAll('li.list-item')
+
+// function arrayList () {
+//     return Array(listitem)
+// }
+// console.log(listitem);
+
+
+// const dataTest = document.querySelectorAll('li')
+
+// const firstData = dataTest[0]
+// console.log(firstData);
+// const secondData = dataTest[1]
+// console.log(secondData);
+// const thirdData = dataTest[2]
+// console.log(thirdData);
+// const fourthData = dataTest[3]
+// console.log(fourthData);
+// const fifthData = dataTest[4]
+// console.log(fifthData);
+
+
+
+// const input = document.querySelector('#secret-login');
+// input.value = 'You were hacked'
+
+
+
+// const white = document.querySelector('.white')
+// console.log(white);
+// white.textContent = "white"
